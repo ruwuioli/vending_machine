5a9af5f866b4ca9d7652766d33b7b94cc5001f2b
Bermet T. <bermettumonbaeva@gmail.com>
2025-12-11 17:05:10 +0600

    implemented PaymentReceiver in CoinAcceptor

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,10 +1,15 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentReceiver {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public void startPayment() {
+        System.out.println("Монет на сумму: " + amount);
     }
 
     public int getAmount() {
@@ -13,5 +18,11 @@ public class CoinAcceptor {
 
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public void topUp() {
+        amount += 10;
+        System.out.println("Вы пополнили баланс на 10");
     }
 }
